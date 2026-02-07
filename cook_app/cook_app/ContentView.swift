a7f1422f61573d1c7ce5191f466d2decefbf6c3b
jaimie-r <jaimie.ren@gmail.com>
2026-02-07 13:43:13 -0600

    makign xcode proj

---
+++ cook_app/cook_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cook_app
+//
+//  Created by Jaimie Ren on 2/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cook_app/cook_app/cook_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  cook_appApp.swift
+//  cook_app
+//
+//  Created by Jaimie Ren on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct cook_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
